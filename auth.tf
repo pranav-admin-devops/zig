22d8e6ef75605f37d576b41a03a8c6d088575882
pranav-admin-devops <pranav.kamble.mail@gmail.com>
2025-04-03 18:02:26 +0530

    Update auth.tf

--- auth.tf
+++ auth.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
 }
-#provider "azurerm" {
-#   features {}
+provider "azurerm" {
+  features {}
 #   subscription_id = var.subscription_id  
 #   client_id       = var.client_id
 #   client_secret   = var.client_secret
 #   tenant_id       = var.tenant_id
-# }
+}
