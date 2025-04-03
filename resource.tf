3e1751346390f6508564c13305723d4dc8a56719
pranav-admin-devops <pranav.kamble.mail@gmail.com>
2025-04-03 18:23:21 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -3,14 +3,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.rg_location
 }
 
-resource "azurerm_virtual_network" "linux-vnet" {
-  name                = "win-network"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
-  depends_on = [ azurerm_resource_group.rg ]
-}
+# resource "azurerm_virtual_network" "linux-vnet" {
+#   name                = "win-network"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   address_space       = ["10.0.0.0/16"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
+#   depends_on = [ azurerm_resource_group.rg ]
+# }
 
 # resource "azurerm_subnet" "linux-sub" {
 #   name                 = "win-subnet"
