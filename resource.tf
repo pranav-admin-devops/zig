fc8db519a8fb809a1cd6cd18616304c20a6e8666
pranav <pranav@blah.com>
2025-04-03 13:44:19 +0530

    Added Nic, VM, NSG

--- resource.tf
+++ resource.tf
@@ -27,3 +27,104 @@ resource "azurerm_public_ip" "linux-pip" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_interface" "linux-nic" {
+  name                = "win-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.linux-sub.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.linux-pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux-vm" {
+  name                = "linux-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = "pranav"
+  admin_password = "Pranav@123"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.linux-nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  provisioner "file" {
+    source = "log.txt"
+    destination = "/tmp/log-backup.txt"
+  }
+  connection {
+    type     = "ssh"
+    user     = self.admin_username
+    password = self.admin_password
+    host     = self.public_ip_address
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo apt update && sudo apt install nginx -y"
+     ]   
+  }
+  provisioner "local-exec" {
+    command = "echo http://${self.public_ip_address} > webserver_ip.txt"
+  }
+}
+
+resource "azurerm_network_security_group" "linux-sg" {
+  name                = "linux-sg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-internet"
+    priority                   = 102
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "linix-association" {
+  network_interface_id      = azurerm_network_interface.linux-nic.id
+  network_security_group_id = azurerm_network_security_group.linux-sg.id
+}
