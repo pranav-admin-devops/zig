03453372f10ab7e37187f5f2b477f915381430ec
pranav-admin-devops <pranav.kamble.mail@gmail.com>
2025-04-03 18:25:50 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -3,22 +3,22 @@ resource "azurerm_resource_group" "rg" {
   location = var.rg_location
 }
 
-# resource "azurerm_virtual_network" "linux-vnet" {
-#   name                = "win-network"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   address_space       = ["10.0.0.0/16"]
-#   dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
-#   depends_on = [ azurerm_resource_group.rg ]
-# }
+resource "azurerm_virtual_network" "linux-vnet" {
+  name                = "win-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
+  depends_on = [ azurerm_resource_group.rg ]
+}
 
-# resource "azurerm_subnet" "linux-sub" {
-#   name                 = "win-subnet"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.linux-vnet.name
-#   address_prefixes     = ["10.0.1.0/24"]
-#   depends_on = [ azurerm_resource_group.rg ]
-# }
+resource "azurerm_subnet" "linux-sub" {
+  name                 = "win-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.linux-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [ azurerm_resource_group.rg ]
+}
 
 # resource "azurerm_public_ip" "linux-pip" {
 #   name                = "win-pip"
